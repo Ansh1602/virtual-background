c10b271fb54871ce910be360f7caa7040c3acd87
Ansh <ansh.khanna16@gmail.com>
2023-02-01 15:17:32 +0530

    green background

--- src/App.js
+++ src/App.js
@@ -1,22 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+// 1. Install dependencies
+// 2. Import dependencies
+// 3. Setup webcam and canvas
+// 4. Define references to those
+// 5. Load handpose
+// 6. Detect function
+// 7. Draw using drawMask
+
+import React, { useEffect, useRef } from "react";
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import Webcam from "react-webcam";
+import "./App.css";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+
+  const runBodysegment = async () => {
+    const net = await bodyPix.load();
+    console.log("BodyPix model loaded.");
+
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100);
+
+
+    // const repeat = () => {
+    //   requestAnimationFrame(detect)
+    // }
+    // detect(net)
+
+  };
+
+
+
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      // * One of (see documentation below):
+      // *   - net.segmentPerson
+      // *   - net.segmentPersonParts
+      // *   - net.segmentMultiPerson
+      // *   - net.segmentMultiPersonParts
+      // const segmentation = await net.segmentPerson(video);
+      const segmentation = await net.segmentPerson(video);
+      // console.log(segmentation);
+
+      // const coloredPartImage = bodyPix.toMask(segmentation);
+      const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
+      const backgroundColor = { r: 0, g: 255, b: 0, a: 255 };
+      const coloredPartImage = bodyPix.toMask(segmentation, foregroundColor, backgroundColor);
+      const opacity = 0.7;
+      const flipHorizontal = false;
+      const maskBlurAmount = 3;
+      const canvas = canvasRef.current;
+
+      bodyPix.drawMask(
+        canvas,
+        video,
+        coloredPartImage,
+        opacity,
+        maskBlurAmount,
+        flipHorizontal
+      );
+    }
+  };
+
+  runBodysegment();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            // position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            // position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
